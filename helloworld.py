32c3aeb44c83c1d12172ce0aaa14b0a2e07359b1
kriwang <kriwang@umich.edu>
2019-09-09 16:32:56 -0400

    First script

---
+++ helloworld.py
@@ -0,0 +1,9 @@
+import random
+
+print("Kristine Wang")
+num1 = random.randint(0,101)
+num2 = random.randint(0,101)
+print(num1)
+print(num2)
+print("Sum =", num1+num2)
+print("Average =", (num1 + num2) / 2)
